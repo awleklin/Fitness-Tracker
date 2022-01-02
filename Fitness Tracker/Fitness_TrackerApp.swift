cd22b20b47eac8ce3f36b97d96ed0a222b31ab02
awleklin <alexwleklinski@gmail.com>
2022-01-01 21:36:17 -0600

    Initial Commit

---
+++ Fitness Tracker/Fitness_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_TrackerApp.swift
+//  Fitness Tracker
+//
+//  Created by Alex Wleklinski on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
