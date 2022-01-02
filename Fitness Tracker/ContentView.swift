4b1b37218eb591293304ea42ae8ae26f26cf30b1
awleklin <alexwleklinski@gmail.com>
2022-01-01 22:45:17 -0600

    Header complete

--- Fitness Tracker/ContentView.swift
+++ Fitness Tracker/ContentView.swift
@@ -9,8 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            RadialGradient(gradient: Gradient(colors: [.blue, .gray]), center: .center, startRadius: 2, endRadius: 650).edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                HStack{
+                    Spacer()
+                    Text("💪").scaleEffect(CGSize(width: 2.0, height: 2.0))
+                    Spacer()
+                    VStack{
+                        Text("Fitness Tracker").font(.title).foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    Text("💪").scaleEffect(CGSize(width: -2.0, height: 2.0))
+                    Spacer()
+                }.padding(20)
+                Text("Pick a workout:")
+                    .foregroundColor(Color.white)
+                HStack{
+                    List {
+                        Group{
+                            Text("Bench Press")
+                            Text("Squat")
+                            Text("Lat Pulldown")
+                        }.listRowBackground(Color.clear)
+                    }.cornerRadius(10)
+                }
+                Spacer()
+            }.padding(20)
+        }
     }
 }
 
